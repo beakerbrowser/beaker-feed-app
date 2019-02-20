e863ceb1e1b29a6d8c96c3eeab8b3d28a13b7889
Paul Frazee <pfrazee@gmail.com>
2019-02-20 08:42:50 -0600

    Update beaker-app-stdlib component usage: custom elements are now automatically defined

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import {LitElement, html} from '/vendor/beaker-app-stdlib/vendor/lit-element/lit-element.js'
-import {AppHeader} from '/vendor/beaker-app-stdlib/js/com/app-header.js'
-import {ProfileInfoCard} from '/vendor/beaker-app-stdlib/js/com/profile-info-card.js'
 import feedMainCSS from '../css/feed-main.css.js'
+import '/vendor/beaker-app-stdlib/js/com/app-header.js'
+import '/vendor/beaker-app-stdlib/js/com/profile-info-card.js'
 import './com/posts-feed.js'
 
 class FeedMain extends LitElement {
@@ -12,14 +12,14 @@ class FeedMain extends LitElement {
   render() {
     return html`
       <link rel="stylesheet" href="/vendor/beaker-app-stdlib/css/fontawesome.css">
-      <app-header
+      <beaker-app-header
         profile-pic-src="/img/tmp-profile.png"
         fontawesome-src="/vendor/beaker-app-stdlib/css/fontawesome.css"
-      ></app-header>
+      ></beaker-app-header>
       <main>
         <div>
           <nav>
-            <profile-info-card></profile-info-card>
+            <beaker-profile-info-card></beaker-profile-info-card>
           </nav>
           <article>
             <posts-feed></posts-feed>
@@ -31,6 +31,4 @@ class FeedMain extends LitElement {
 }
 FeedMain.styles = feedMainCSS
 
-AppHeader.register()
-ProfileInfoCard.register()
 customElements.define('feed-main', FeedMain)
